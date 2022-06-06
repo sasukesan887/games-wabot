012d78eac4881a21ad85720df8b36028f377aa79
sasukesan887 <106885229+sasukesan887@users.noreply.github.com>
2022-06-06 18:01:57 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,29 +2,29 @@ global.DeveloperMode = 'false' //true Or false
 global.linkGC = ['https://chat.whatsapp.com/Lb4Emjih98rBiCZiZoS2eM', 'https://chat.whatsapp.com/FrluVyY5kBRIhY6yKfH4DI']
 global.Owner = {
   // use the phone number with the country code, for example Indonesia '62' America '1' etc
-  '62857139649630': { // put your number here
+  '212688905765': { // put your number here
     name: 'Owner Metro Bot/Creator games-wabot',
     isDev: false, // if true this number will send if bot have bug and developer mode true
     isReport: false, // if true this number will send if user use command /report or /join
     isCreator: true // if true this number will send if user use command /owner or /creator
   },
-  '6281390658325': { // put your number here
-    isDev: true, // if true this number will send if bot have bug and developer mode true
-    isReport: true, // if true this number will send if user use command /report or /join
-    isCreator: false // if true this number will send if user use command /owner or /creator
+  '212688905765': { /212688905765/ put your number here
+    isDev: true, /212688905765/ if true this number will send if bot have bug and developer mode true
+    isReport: true, /212688905765/ if true this number will send if user use command /report or /join
+    isCreator: false /212688905765/ if true this number will send if user use command /owner or /creator
   },
-  '6282256115584': { // put your number here
-    name: 'Dave',
-    isDev: false, // if true this number will send if bot have bug and developer mode true
-    isReport: true, // if true this number will send if user use command /report or /join
-    isCreator: false // if true this number will send if user use command /owner or /creator
+  '212688905765': { /212688905765/ put your number here
+    name: 'sasuke',
+    isDev: false, /212688905765/ if true this number will send if bot have bug and developer mode true
+    isReport: true, /212688905765/ if true this number will send if user use command /report or /join
+    isCreator: false /212688905765/ if true this number will send if user use command /owner or /creator
   },
-  '6285713964963': {
-    isReport: true // if true this number will send if user use command /report or /join
+  '212688905765': {
+    isReport: true /212688905765/ if true this number will send if user use command /report or /join
   },
   '13479805233': {} // ignore if you don't want anything, like name dll
 }
-global.mods = ['13479805233'] // Want some help?
+global.mods = ['212688905765'] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
@@ -55,8 +55,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'wa.me/6285713964963'
-global.author = 'Metro Bot Is The Best'
+global.packname = '⚜︎╎dᥱᥲdᥣყ﹝⛩️﹞𝚂𝚃𝙸𝙲𝙺𝙴𝚁╎⚜︎'
+global.author = 'sasuke san'
 
 global.multiplier = 39 // The higher, The harder levelup
 
